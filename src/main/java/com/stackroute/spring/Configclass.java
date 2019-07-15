aec8c907a4a5af067430b7ad6ab8c557ad75f8db
akhila <bakhila36@gmail.com>
2019-07-15 09:57:32 +0530

    annotations task 3

--- src/main/java/com/stackroute/spring/Configclass.java
+++ src/main/java/com/stackroute/spring/Configclass.java
@@ -1,51 +1,32 @@
 package com.stackroute.spring;
 
-import com.stackroute.spring.Actor;
-import com.stackroute.spring.Movie;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+
 
 @Configuration
 public class Configclass {
 
-    //	Movie Beans
-    @Bean(name = {"movieObj1", "movieObject"})
-    public Movie movieBean (){
-        Movie movie = new Movie(actorObj1());
-        return movie;
+//	Movie Beans
+
+
+    @Bean
+    public Movie movieObj (){
+        return new Movie();
+    }
+
+
+
+    @Bean
+    public Actor actorObj (){
+        return new Actor("bhanu","female",39);
     }
 
     @Bean
-    public Movie movieObj2 (){
-        Movie movie = new Movie(actorObj2());
-        return movie;
+    public Actor actor (){
+        return new Actor("jaya","female",46);
     }
-
-    @Bean
-    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public Movie movieObj3 (){
-        Movie movie = new Movie(actorObj3());
-        return movie;
-    }
-
-    //	Actor Beans
-    @Bean
-    public Actor actorObj1 (){
-        return new Actor("akhila","female",22);
-    }
-
-    @Bean
-    public Actor actorObj2 (){
-        return new Actor("bhanu","female",16);
-    }
-
-    @Bean
-    public Actor actorObj3 (){
-        return new Actor("Shastra","female",20);
-    }
-
-
 
 }
+
--- src/main/java/com/stackroute/spring/Movie.java
+++ src/main/java/com/stackroute/spring/Movie.java
@@ -1,19 +1,29 @@
 package com.stackroute.spring;
 
-public class Movie {
+import org.springframework.beans.factory.annotation.Autowired;
 
-    private Actor actor;
 
-    public Movie() {
+    public class Movie {
+
+        //	@Autowired
+        private Actor actor;
+
+        public Movie() {
+        }
+        public Movie(Actor actor) {
+            this.actor = actor;
+
+        }
+
+        @Autowired
+        public void setActor(Actor actor) {
+            this.actor = actor;
+
+
+        }
+
+        public void display()
+        {
+            System.out.println("Actor Name: "+actor.getName()+" Gender: "+actor.getGender()+" Age: "+actor.getAge() );
+        }
     }
-
-    public Movie(Actor actor) {
-        this.actor = actor;
-    }
-
-    public void display()
-    {
-        System.out.println("Actor Name: "+actor.getName()+" Gender: "+actor.getGender()+" Age: "+actor.getAge() );
-    }
-}
-
