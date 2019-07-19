06e1e0ffbd683435f4b13a0a9a6c746483e7435d
akhila <bakhila36@gmail.com>
2019-07-19 12:33:39 +0530

    issues resolved

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -2,19 +2,32 @@ package com.stackroute;
 
 import com.stackroute.spring.Movie;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 
 public class Main {
     public static void main( String[] args )
     {
+        //using application context
         ApplicationContext context = new AnnotationConfigApplicationContext("com.stackroute.spring");
         Movie movie1 = context.getBean("movieBean",Movie.class);
         movie1.display();
 
+        //bean factory
         BeanFactory factory = new AnnotationConfigApplicationContext("com.stackroute.spring");
         Movie movie2 = factory.getBean("movieBean",Movie.class);
         movie2.display();
+
+//        //beandefinition reader and bean definition registry
+//        DefaultListableBeanFactory beanFactory=new DefaultListableBeanFactory();
+//        BeanDefinitionReader reader=new XmlBeanDefinitionReader(beanFactory);
+//        reader.loadBeanDefinitions(new FileSystemResource("src/main/java/com/stackroute/spring/Configclass"));
+//        Movie movie3=context.getBean("movieBean",Movie.class);
+//        movie3.display();
 
 
     }
